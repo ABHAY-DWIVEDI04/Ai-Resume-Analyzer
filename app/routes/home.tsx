7bdcc1d1f8b3e38571e934df1ae5d0c796bbe45b
Abhay Dwivedi <abhaydwivedi4504@gmail.com>
2025-09-23 23:41:38 +0530

    changes made

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,34 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+// import { resumes } from "constants";
+// Update the import path below to the correct relative location of your constants file
+import { resumes } from "../../constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResumeAnly" },
+    { name: "description", content: "Great feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Rating</h1>
+          <h2>Review your submissions and check AI-powered feedback</h2>
+        </div>
+      </section>
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume: Resume) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
